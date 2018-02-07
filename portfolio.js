b8e8b62905d65fd3d48ef9fe793e7cba1cf3e803
Marius <mdelininkaitis@gmail.com>
2018-02-07 15:31:38 +0200

    c

--- portfolio.js
+++ portfolio.js
@@ -25,6 +25,11 @@ function Element() {
     }
   };
 
+  this.addElements = function(Id, elementsFromObject) {
+    for(let x in elementsFromObject) {
+      getElemId(Id).innerHTML += elementsFromObject[x];
+    }
+  };
   //should add elements to already existing IDs or Classes:
   // this.addElements = function(Id, styleFromObject) {
   //   for(let x in styleFromObject) {
@@ -71,15 +76,27 @@ for(let x in pageBody.style) {
 
 //navbar
 let navData = {
-  style: {width: "100%", height: "80px", backgroundColor: "red", textAlign: "center", lineHeight: "80px"},
-  colors: {color: "white"}
+  styleParent: {
+    width: "100%", height: "60px", backgroundColor: "#87509c", textAlign: "center",
+    lineHeight: "60px", paddingTop: "60px", color: "white"
+  },
+  html: {
+    container: "<div class='container'>",
+    links: "<ul id='main_links'><li style='display: inline-block;'>Home</li><li style='display: inline-block;'>About</li><li>Work</li><li>Blog</li><li>Contact</li>",
+    containerEnd: "</div>",
+  },
+  styleHtml: {
+    // main_links: {
+    //
+    // }
+  }
 };
 
 let nav = new Element();
-nav.createElem('div', 'page', 'nav', '<b>Hello World!</b>');
-nav.createElem('div', 'page', 'asdf');
-nav.addStyle('nav', navData.style);
-nav.addStyle('nav', navData.colors);
+nav.createElem('nav', 'page', 'nav');
+// nav.createElem('div', 'page', 'asdf');
+nav.addStyle('nav', navData.styleParent);
+nav.addElements('nav', navData.html);
 
 //
 // let testinis = {
