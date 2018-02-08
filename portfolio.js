83b441dd26b5e9de5e0faa2fb07ae04de5d3fe49
Marius <mdelininkaitis@gmail.com>
2018-02-08 20:10:06 +0200

    hero

--- portfolio.js
+++ portfolio.js
@@ -5,13 +5,13 @@ function getElemId(id) {
   return document.getElementById(id);
 }
 
-function elementStyle(element) {
-  return document.getElementById(element).style;
+function selectElem(element) {
+  return document.querySelectorAll(element);
 }
 
-// creates new element with ID in any parent element ID and adds innerHTML
 function Element() {
 
+  // creates new element with ID in any parent element ID and adds innerHTML
   this.createElem = function(element, parentElement, newElemId, innerHtml='') {
     let elem = document.createElement(element);
     elem.setAttribute("id", newElemId);
@@ -20,27 +20,26 @@ function Element() {
   };
 
   this.addStyle = function(Id, styleFromObject) {
-    for(let x in styleFromObject) {
-      elementStyle(Id)[x] = styleFromObject[x];
+    let elem = selectElem(Id);  // gets array of elements
+    for(let i = 0; i < elem.length; i++) { // loops elem
+      for(let x in styleFromObject) {
+        elem[i].style[x] = styleFromObject[x];  //ads style to elements in elem
+      }
     }
   };
 
-  this.addElements = function(Id, elementsFromObject) {
-    for(let x in elementsFromObject) {
-      getElemId(Id).innerHTML += elementsFromObject[x];
+
+  this.addElements = function(WhereToAdd, whatToAdd) {
+    let dest = selectElem(WhereToAdd);
+    for(let i = 0; i < dest.length; i++) {
+      for(let x in whatToAdd) {
+        dest[i].innerHTML += whatToAdd[x];
+      }
     }
   };
-  //should add elements to already existing IDs or Classes:
-  // this.addElements = function(Id, styleFromObject) {
-  //   for(let x in styleFromObject) {
-  //     elementStyle(Id)[x] = styleFromObject[x];
-  //   }
-  // };
+
 
 }
-
-
-
 
 
 
@@ -51,8 +50,8 @@ function Element() {
 
 //add link rel to document head
 (function(fileName){
-  var head = document.head
-  , link = document.createElement("link")
+  let head = document.head,
+      link = document.createElement("link");
 
 link.type = "text/css"
 link.rel = "stylesheet"
@@ -64,47 +63,106 @@ head.appendChild(link)
 
 
 let pageBody = {
-  style: {margin: "0 auto", fontFamily: "'Titillium Web', sans-serif"}
+  // html: {
+  //   container: { data: "<div class='main_container'></div>" }
+  // },
+  style: {
+    body: { margin: "0 auto", fontFamily: "'Titillium Web', sans-serif" },
+    main_container: { maxWidth: "1280px", margin: "0 auto" }
+  }
 };
 //add style to body from pageBody object
-for(let x in pageBody.style) {
-  document.body.style[x] = pageBody.style[x];
+for(let x in pageBody.style.body) {
+  document.body.style[x] = pageBody.style.body[x];
 }
 
+// let page = new Element();
+//
+// page.addElements('#page', pageBody.html.container);
+// page.addStyle('#page', pageBody.style.body);
 
 
 
-//navbar
+
+
+// NAVBAR
 let navData = {
-  styleParent: {
-    width: "100%", height: "60px", backgroundColor: "#87509c", textAlign: "center",
-    lineHeight: "60px", paddingTop: "60px", color: "white"
-  },
   html: {
-    container: "<div class='container'>",
-    links: "<ul id='main_links'><li style='display: inline-block;'>Home</li><li style='display: inline-block;'>About</li><li>Work</li><li>Blog</li><li>Contact</li>",
-    containerEnd: "</div>",
+    container: {data: "<div class='container'></div>"},
+    logo: { data: "<div class='logo_container'><b>Logo</b></div>" },
+    links: { data: "<div class='nav_links'><ul><li>Home</li><li>About</li><li>Work</li><li>Blog</li><li>Contact</li></ul></div>" },
   },
-  styleHtml: {
-    // main_links: {
-    //
-    // }
+  style: {
+    page: {
+      width: "100%", height: "60px", backgroundColor: "#87509c", textAlign: "center",
+      lineHeight: "60px", paddingTop: "60px", color: "white"
+    },
+    container: {
+      maxWidth: "1198px", margin: "0 20px"
+    },
+    logo_container: {
+      float: "left"
+    },
+    nav_links: {
+      float: "right"
+    },
+    nav_links_li: {
+      display: "inline-block", marginLeft: "15px", cursor: "pointer"
+    }
   }
 };
 
-let nav = new Element();
-nav.createElem('nav', 'page', 'nav');
-// nav.createElem('div', 'page', 'asdf');
-nav.addStyle('nav', navData.styleParent);
-nav.addElements('nav', navData.html);
 
-//
-// let testinis = {
-//   a: 1,
-//   b: 2,
-//   c: 3
-// };
-//
-// for(let x in testinis) {
-//   console.log(testinis[x]);
-// }
+let nav = new Element();
+              //element, parentElement, newElemId, innerHtml=''
+nav.createElem('nav', 'page', 'nav');
+
+                //WhereToAdd, whatToAdd
+nav.addElements('#nav', navData.html.container);
+nav.addElements('.container', navData.html.logo);
+nav.addElements('.container', navData.html.links);
+nav.addStyle('#nav', navData.style.page);
+nav.addStyle('.container', navData.style.container);
+nav.addStyle('.logo_container', navData.style.logo_container);
+nav.addStyle('.nav_links', navData.style.nav_links);
+nav.addStyle('.nav_links > ul > li', navData.style.nav_links_li);
+
+
+// HERO
+let heroData = {
+  html: {
+    container: {data: "<div class='container'></div>"},
+    logo: { data: "<div class='logo_container'><b>Logo</b></div>" },
+    links: { data: "<div class='nav_links'><ul><li>Home</li><li>About</li><li>Work</li><li>Blog</li><li>Contact</li></ul></div>" },
+  },
+  style: {
+    page: {
+      width: "100%", height: "600px", backgroundColor: "#87509c", textAlign: "center",
+      // lineHeight: "60px", paddingTop: "60px", color: "white"
+    },
+    container: {
+      maxWidth: "1198px", margin: "0 20px"
+    },
+    logo_container: {
+      float: "left"
+    },
+    nav_links: {
+      float: "right"
+    },
+    nav_links_li: {
+      display: "inline-block", marginLeft: "15px", cursor: "pointer"
+    }
+  }
+};
+
+let hero = new Element();
+hero.createElem('section', 'page', 'hero');
+                //WhereToAdd, whatToAdd
+nav.addElements('#hero', heroData.html.container);
+nav.addElements('.container', heroData.html.logo);
+nav.addElements('.container', heroData.html.links);
+nav.addStyle('#nav', heroData.style.page);
+nav.addStyle('.container', heroData.style.container);
+nav.addStyle('.logo_container', heroData.style.logo_container);
+nav.addStyle('.nav_links', heroData.style.nav_links);
+nav.addStyle('.nav_links > ul > li', heroData.style.nav_links_li);
